c019e3618bb8adc1fd3c7a6b7c433110488fe0f2
Kaye <kay22ng@yahoo.com>
2018-08-31 13:29:46 -0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
+//create server using http
+var server = http.createServer(app);
+//configure server to use socketio
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Server is up on ${port}`);
+});
+io.on('connection', socket => {
+	console.log('New connection');
+
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
 });
